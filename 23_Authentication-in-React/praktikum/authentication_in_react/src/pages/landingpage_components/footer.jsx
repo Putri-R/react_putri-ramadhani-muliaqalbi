3a425eb0842d1688506b64cd7d19f81c6ea0857d
Putri-R <putrirahmadhani005@gmail.com>
2023-10-17 17:05:32 +0700

    upload tugas authentication in react

---
+++ 23_Authentication-in-React/praktikum/authentication_in_react/src/pages/landingpage_components/footer.jsx
@@ -0,0 +1,76 @@
+export default function Footer(){
+    return(
+        <>
+        <footer>
+            <div className="footer-class">
+            <div className="footer-group">
+                <h1>Arsha</h1>
+                <p>
+                A108 Adam Street <br />
+                New York, NY 535022
+                <br />
+                United States <br />
+                <br />
+                <span>Phone:</span> +1 5589 55488 55
+                <br />
+                <span>Email:</span> info@example.com
+                </p>
+            </div>
+            <div className="footer-group">
+                <h4>Useful Links</h4>
+                <div className="child-group">
+                <a href="">Home</a>
+                <br />
+                <a href="">About</a>
+                <br />
+                <a href="">Services</a>
+                <br />
+                <a href="">Terms of service</a>
+                <br />
+                <a href="">Privacy policy</a>
+                <br />
+                </div>
+            </div>
+            <div className="footer-group">
+                <h4>Our Services</h4>
+                <div className="child-group">
+                <a href="">Web Design</a>
+                <br />
+                <a href="">Web Development</a>
+                <br />
+                <a href="">Product Management</a>
+                <br />
+                <a href="">Marketing</a>
+                <br />
+                <a href="">Graphic Design</a>
+                <br />
+                </div>
+            </div>
+            <div className="footer-group">
+                <h4>Our Social Networks</h4>
+                <p>
+                Cras fermentum odio eu feugiat lide par naso tierra videa magna derita
+                valies
+                </p>
+                <div className="sosial-network">
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                </div>
+            </div>
+            </div>
+        </footer>
+        {/* copyright */}
+        <div className="copyright">
+            <p className="copyright-2">
+            © Copyright <span>Arsha</span>. All Rights Reserved
+            </p>
+            <p className="copyright-3">
+            Designed by <span>BootstrapMade</span>
+            </p>
+        </div>
+        </>
+    )
+}
